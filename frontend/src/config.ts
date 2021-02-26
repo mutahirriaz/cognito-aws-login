e4864ffc29db0d05dffeb9d52a6effa371d3f8c8
mutahirriaz <64969144+mutahirriaz@users.noreply.github.com>
2021-02-27 00:50:25 +0500

    frontend

---
+++ frontend/src/config.ts
@@ -0,0 +1,10 @@
+const config = {
+    domainUrl: "https://my-awesome-app.auth.us-east-1.amazoncognito.com",
+    clientId: "6uuos0u87lfml34f8h20anii3a",
+    loginRedirectUri: "http://localhost:8000/dashboard",
+    grant_type: "authorization_code",
+    logoutUri: "http://localhost:8000",
+    clientSecret: "1sf9rrakkpm50tt95ipihg5thv41vr9166mkdf2fddmslimqkkl4",
+}
+
+export default config
